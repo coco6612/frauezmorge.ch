48bf5423449c0073c28ddc5f37373259a2383968
Corinne Dascenzo <coco6612@gmail.com>
2024-09-16 11:25:46 +0200

    Add shadows and links

--- site/snippets/event-thumbnail.php
+++ site/snippets/event-thumbnail.php
@@ -1,9 +1,11 @@
 <?= css('assets/css/snippets/event-thumbnail.css') ?>
-<div class="event-thumbnail">
-  <?php if($image = $event->eventImage()->toFile()): ?>
-    <img class="rellax" src="<?= $image->url() ?>" alt="" data-rellax-speed="0" data-rellax-zindex="10">
-  <?php endif ?>
-  <div class="thumbnail-date"><?= $event->date()->toDate('d.m.Y') ?></div>
-  <h3 class="thumbnail-title"><?= $event->name() ?></h3>
-  <div class="thumbnail-subtitle"><?= $event->subtitle() ?></div>
-</div>
+<a class="event-thumbnail-link" href="<?= $event->url() ?>">
+  <div class="event-thumbnail">
+    <?php if($image = $event->eventImage()->toFile()): ?>
+      <img class="rellax" src="<?= $image->url() ?>" alt="" data-rellax-speed="0" data-rellax-zindex="10">
+    <?php endif ?>
+    <div class="thumbnail-date"><?= $event->date()->toDate('d.m.Y') ?></div>
+    <h3 class="thumbnail-title"><?= $event->name() ?></h3>
+    <div class="thumbnail-subtitle"><?= $event->subtitle() ?></div>
+  </div>
+  </a>
