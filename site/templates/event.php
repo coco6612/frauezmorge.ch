14543533dc4f3541fd35baa02196960490edff34
Corinne Dascenzo <coco6612@gmail.com>
2024-11-18 08:29:54 +0100

    Add event notes

--- site/templates/event.php
+++ site/templates/event.php
@@ -2,6 +2,10 @@
 <div class="content-wrapper"id="headliners">
   <div class="content-max-width">
     <?php snippet('event-teaser', ['event' => $page, 'subtitle' => 'Frauezmorge', 'type' => 'details']) ?>
+    <div class="event-notes">
+      <h2><?= $page->notesTitle()->text() ?></h2>
+      <p><?= $page->notes()->kirbytext() ?></p>
+    </div>
   </div>
 </div>
 <?php snippet('footer') ?>
